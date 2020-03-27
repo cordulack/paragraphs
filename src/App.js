38abaed73bab5cdfaa5042ad64dafd0061e5ca54
Evan Cordulack <ecordulack@gmail.com>
2020-03-27 13:19:23 -0700

    Added a few tests using Jest and Enzyme

--- src/App.js
+++ src/App.js
@@ -84,6 +84,7 @@ function TextGenerator() {
       <label>
         Number of Paragraphs
         <input
+          className="paragraph-number"
           type="number"
           min={1}
           value={numberOfParagraphs}
@@ -94,6 +95,7 @@ function TextGenerator() {
       <div className="menu-item">
         <label>Min. Words in Paragraph
         <input
+          className="minWordCount"
           type="number"
           min="0"
           value={minWordCount}
@@ -101,17 +103,33 @@ function TextGenerator() {
         />
         </label>
       </div>
-      <div className="menu-item"><label>Paragraph Numbers<input type="checkbox" onChange={ e => setShowParagraphNumbers(e.target.checked)}/></label></div>
-      <div className="menu-item"><label>Word Count<input type="checkbox" onChange={ e => setShowWordCount(e.target.checked)}/></label></div>
+      <div className="menu-item">
+        <label>Paragraph Numbers
+          <input
+            className="show-paragraph-numbers"
+            type="checkbox"
+            onChange={ e => setShowParagraphNumbers(e.target.checked)}
+            value={showParagraphNumbers}/>
+        </label>
+      </div>
+      <div className="menu-item">
+        <label>Word Count
+          <input
+            className="show-word-count"
+            type="checkbox"
+            onChange={ e => setShowWordCount(e.target.checked)}
+            value={showWordCount}/>
+        </label>
+      </div>
       <div className="buttons">
-        <button onClick={generateText}>Refresh Paragraphs</button>
+        <button className="refresh-button" onClick={generateText}>Refresh Paragraphs</button>
         <button className="copy-button" onClick={handleCopyButtonClick}>
           <svg className="icon-done hide" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"/><path fill="white" d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"/></svg>
           <span className="copy-button-text">Copy</span>
         </button>
       </div>
     </div>
-      <div className={generatedText.length !== 0 ? "generated-text u-bg-image-none" :"generated-text"}>
+      <div className={generatedText.length !== 0 ? "generated-text u-bg-image-none" : "generated-text"}>
         {generatedText.map( (paragraph, i) => {
           return <p key={i}>{paragraph}</p>
         })}
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,96 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { mount } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import TextGenerator from './App';
+
+test('Paragraph Number Input should initially contain a value of 25', () => {
+  const textGeneratorWrapper = mount(<TextGenerator />);
+  const numberOfParagraphs = textGeneratorWrapper.find('.paragraph-number').props().value;
+  expect(numberOfParagraphs).toStrictEqual(25);
+  textGeneratorWrapper.unmount();
 });
+
+test('Minimum Word Count should initially contain a value of 20', () => {
+  const textGeneratorWrapper = mount(<TextGenerator />);
+  const numberOfParagraphs = textGeneratorWrapper.find('.minWordCount').props().value;
+  expect(numberOfParagraphs).toStrictEqual(20);
+  textGeneratorWrapper.unmount();
+});
+
+test('Show Paragraph Numbers should start off unchecked', () => {
+  const textGeneratorWrapper = mount(<TextGenerator />);
+  const showParagraphNumbers = textGeneratorWrapper.find('.show-paragraph-numbers').props().value;
+  expect(showParagraphNumbers).toBe(false);
+  textGeneratorWrapper.unmount();
+});
+
+test('Show WordCount should start off unchecked', () => {
+  const textGeneratorWrapper = mount(<TextGenerator />);
+  const showWordCount = textGeneratorWrapper.find('.show-word-count').props().value;
+  expect(showWordCount).toBe(false);
+  textGeneratorWrapper.unmount();
+});
+
+test('Example Text should initially have 25 paragraphs', () => {
+  const textGeneratorWrapper = mount(<TextGenerator />);
+  const paragraphs = textGeneratorWrapper.find('p');
+  expect(paragraphs).toHaveLength(25);
+  textGeneratorWrapper.unmount();
+});
+
+test('Example text should change after clicking the Refresh Paragraphs buttons', () => {
+  const textGeneratorWrapper = mount(<TextGenerator />);
+  const originalParagraphs = textGeneratorWrapper.find('.generated-text');
+
+  textGeneratorWrapper.find('.refresh-button').simulate('click');
+
+  const newParagraphs = textGeneratorWrapper.find('.generated-text');
+  expect(originalParagraphs).not.toEqual(newParagraphs);
+  textGeneratorWrapper.unmount();
+});
+
+test('Example text should contain the number of paragraphs we have entered', () => {
+  const textGeneratorWrapper = mount(<TextGenerator/>);
+  textGeneratorWrapper.find('.paragraph-number').simulate('change', { target: {value: 5}});
+
+  // Simulate a click to refresh the paragraphs to generate the number we want
+  textGeneratorWrapper.find('.refresh-button').simulate('click');
+
+  const numberOfParagraphs = textGeneratorWrapper.find('p').length;
+  expect(numberOfParagraphs).toEqual(5);
+});
+
+test('Example text should contain paragraph numbers when Show Paragraph Numbers is checked', () => {
+  const textGeneratorWrapper = mount(<TextGenerator />);
+  // Since this checkbox sets the state on change (instead of just doing something when clicked, we use 'change')
+  textGeneratorWrapper.find('.show-paragraph-numbers').simulate('change', { target: {checked: true}});
+
+  // Simulate a click to refresh the paragraphs that should now include a paragraph number
+  textGeneratorWrapper.find('.refresh-button').simulate('click');
+
+  // Get all paragraphs, take the first one, then grab its text
+  const firstParagraphText = textGeneratorWrapper.find('p').at(0).text();
+
+  // Check to see if the first paragraph has our formatted paragraph number
+  expect(firstParagraphText).toEqual(expect.stringContaining('1 - '));
+
+  textGeneratorWrapper.unmount();
+});
+
+test('Example text should contain word count when Show WordCount Numbers is checked', () => {
+  const textGeneratorWrapper = mount(<TextGenerator />);
+  // Since this checkbox sets the state on change (instead of just doing something when clicked, we use 'change')
+  textGeneratorWrapper.find('.show-word-count').simulate('change', { target: {checked: true}});
+
+  // Simulate a click to refresh the paragraphs that should now include a paragraph number
+  textGeneratorWrapper.find('.refresh-button').simulate('click');
+
+  const firstParagraphText = textGeneratorWrapper.find('p').at(0).text();
+
+  // Check to see if the first paragraph has our formatted word count number
+  expect(firstParagraphText).toEqual(expect.stringMatching(/\(\d+\)/));
+
+  textGeneratorWrapper.unmount();
+});
+
+
