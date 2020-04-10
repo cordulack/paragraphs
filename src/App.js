4398e37b15677ef89d9fb3e02b1c77812b5c7ce1
Evan Cordulack <ecordulack@gmail.com>
2020-04-10 11:52:28 -0700

    Added basic functionality for query string parameters

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React, { useState, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useLocation,
+  useHistory,
+} from "react-router-dom";
 
 import { baseText } from './baseText.js';
 
 function TextGenerator() {
-  const [numberOfParagraphs, setNumberOfParagraphs] = useState(25);
-  const [minWordCount, setMinWordCount] = useState(20);
-  const [showParagraphNumbers, setShowParagraphNumbers] = useState(false);
-  const [showWordCount, setShowWordCount] = useState(false);
+  let query = useQuery();
+  let history = useHistory();
+
+  const urlParams = getURLParamValues();
+
+  const [numberOfParagraphs, setNumberOfParagraphs] = useState(urlParams.numberOfParagraphs);
+  const [minWordCount, setMinWordCount] = useState(urlParams.minWordCount);
+  const [showParagraphNumbers, setShowParagraphNumbers] = useState(urlParams.showParagraphNumbers);
+  const [showWordCount, setShowWordCount] = useState(urlParams.showWordCount);
   const [generatedText, setGeneratedText] = useState([]);
+
+  // Get the current location.
+  const location = history.location;
+
+  history.listen(() => {
+     const differentURLParams = getURLParamValues();
+     setNumberOfParagraphs(differentURLParams.numberOfParagraphs);
+     setMinWordCount(differentURLParams.minWordCount);
+     setShowParagraphNumbers(differentURLParams.showParagraphNumbers);
+     setShowWordCount(differentURLParams.showWordCount);
+  });
 
   useEffect(()=> {
     generateText();
   }, []);
 
-  useEffect(()=> {
-    document.addEventListener("keydown", e => {
-      if(e.key === 'Enter'){
-        generateText();
-      }
-    }, false);
-  }, []);
-
+  function getURLParamValues() {
+    return {
+      'numberOfParagraphs': query.get('paragraphs') ? parseInt(query.get('paragraphs')) : 20,
+      'minWordCount': query.get('words') ? parseInt(query.get('words')) : 20,
+      'showParagraphNumbers' : query.get('pnumbers') === "1",
+      'showWordCount': query.get('wordcount') === "1",
+    };
+  }
   function generateText() {
     const paragraphs = generateParagraphs(baseText, numberOfParagraphs);
     setGeneratedText(paragraphs);
@@ -77,6 +101,14 @@ function TextGenerator() {
     document.querySelector('.icon-done').classList.toggle('hide');
   }
 
+  function changeURL() {
+    query.set('paragraphs', numberOfParagraphs);
+    query.set('words', minWordCount);
+    query.set('pnumbers', `${showParagraphNumbers ? '1': '0'}`);
+    query.set('wordcount', `${showWordCount ? '1' : '0'}`);
+    window.history.replaceState({}, '', `${location.pathname}?${query}`);
+  }
+
   return(
     <div className="text-generator">
       <div className="u-list-style-none menu">
@@ -101,16 +133,20 @@ function TextGenerator() {
         />
         </label>
       </div>
-      <div className="menu-item"><label>Paragraph Numbers<input type="checkbox" onChange={ e => setShowParagraphNumbers(e.target.checked)}/></label></div>
-      <div className="menu-item"><label>Word Count<input type="checkbox" onChange={ e => setShowWordCount(e.target.checked)}/></label></div>
+      <div className="menu-item"><label>Paragraph Numbers<input type="checkbox" checked={showParagraphNumbers} onChange={ e => setShowParagraphNumbers(e.target.checked)}/></label></div>
+      <div className="menu-item"><label>Word Count<input type="checkbox" checked={showWordCount} onChange={ e => setShowWordCount(e.target.checked)}/></label></div>
       <div className="buttons">
-        <button onClick={generateText}>Refresh Paragraphs</button>
+        <button onClick={ () => {
+          generateText();
+          changeURL();
+        }
+        }>Refresh Paragraphs</button>
         <button className="copy-button" onClick={handleCopyButtonClick}>
           <svg className="icon-done hide" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"/><path fill="white" d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"/></svg>
           <span className="copy-button-text">Copy</span>
         </button>
       </div>
-    </div>
+      </div>
       <div className={generatedText.length !== 0 ? "generated-text u-bg-image-none" :"generated-text"}>
         {generatedText.map( (paragraph, i) => {
           return <p key={i}>{paragraph}</p>
@@ -122,8 +158,30 @@ function TextGenerator() {
 
 function App() {
   return (
-    <div className="App">
-      <TextGenerator className='text-generator'/>
+    <Router>
+      <div className="App">
+        <Link to="/">Paragraphs</Link> | <Link to="/about">About</Link>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <TextGenerator className='text-generator'/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
     </div>
   );
 }
